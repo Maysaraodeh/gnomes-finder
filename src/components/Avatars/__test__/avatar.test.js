0c2659a93a79e51776e3917df0172768d030fbb9
Maysaraodeh <maso-505@hotmail.com>
2020-07-25 13:14:57 +0200

    Add test
    add new tests for avatar component render

---
+++ src/components/Avatars/__test__/avatar.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Avatar from '../Avatar';
+import { render } from '@testing-library/react';
+import { stubConsoleError } from '../../../helpers/testUtils';
+import sinon from 'sinon';
+import { gnomeAvatar } from './data';
+
+stubConsoleError();
+
+test('should render Avatar component with no errors', () => {
+  const { getByText, getByTestId } = render(<Avatar {...gnomeAvatar} />);
+  expect(getByTestId('avatar').classList.contains('tooltip')).toBe(true);
+  expect(getByText(/Gnome/i)).toBeInTheDocument();
+  expect(getByTestId('avatar-name')).toHaveTextContent('Gnome');
+  expect(getByTestId('avatar-img').src).toEqual('https://imageurl.com/');
+  expect(getByTestId('avatar-icons')).toBeInTheDocument();
+  expect(getByTestId('avatar-professions')).toBeInTheDocument();
+  sinon.assert.notCalled(console.error);
+});
+
+it('should produce missing props error', () => {
+  render(<Avatar />);
+  sinon.assert.called(console.error);
+});
---
+++ src/components/Avatars/__test__/data.js
@@ -0,0 +1,9 @@
+export const gnomeAvatar = {
+  name: 'Gnome',
+  weight: 1,
+  height: 1,
+  age: 1,
+  professions: ['metal', 'wood'],
+  thumbnail: 'https://imageurl.com',
+  id: 1,
+};
