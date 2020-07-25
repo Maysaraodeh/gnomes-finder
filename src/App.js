8e138e8ad2934edb9c5c55803ead44585d2ad2e0
Maysaraodeh <maso-505@hotmail.com>
2020-07-25 03:56:02 +0200

    Add layout
    main app layout of navbar and home view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import MainLayout from 'layout';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={MainLayout} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
